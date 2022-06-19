3a49d9a1f617329e8150553450a5558610ea6795
Sudharshan1246 <107745985+Sudharshan1246@users.noreply.github.com>
2022-06-19 16:02:21 +0530

    powet

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -38,7 +38,7 @@ async def save_group(bot, message):
             return
         buttons = [[
             InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('📢 Updates', url='https://t.me/TechnoMindzchat')
+            InlineKeyboardButton('📢 Updates', url='https://t.me/MCU_Linkz')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
@@ -53,7 +53,7 @@ async def save_group(bot, message):
                         await (temp.MELCOW['welcome']).delete()
                     except:
                         pass
-                temp.MELCOW['welcome'] = await message.reply(f"<b>Hello 👋, {u.mention}, Welcome to {message.chat.title} \nHow Are you ?\nJoin Your Freiends Here\nAlso Join Our Chatting Group @TechnoMindzChat</b>")
+                temp.MELCOW['welcome'] = await message.reply(f"<b>Hello 👋, {u.mention}, Welcome to {message.chat.title} \nHow Are you ?</b>")
 
 
 @Client.on_message(filters.command('leave') & filters.user(ADMINS))
@@ -67,7 +67,7 @@ async def leave_a_chat(bot, message):
         chat = chat
     try:
         buttons = [[
-            InlineKeyboardButton('Support', url=f'https://t.me/technomindzchat')
+            InlineKeyboardButton('Support', url=f'https://t.me/MCU_Linkz')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await bot.send_message(
