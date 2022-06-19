1e6a87c8115aa6f624098c27cf2554209d9062b6
Sudharshan1246 <107745985+Sudharshan1246@users.noreply.github.com>
2022-06-19 16:09:09 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -4,7 +4,7 @@ class script(object):
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/Technomindzyt><b><i>RAVINANDAN</i></b></a>
+✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/Technomindzyt><b><i>:⋆🎀 𝑅𝒶𝓋𝒾𝓃𝒶𝓃𝒹𝒶𝓃 🎀⋆:</i></b></a>
 ✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
 ✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
 ✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
@@ -13,7 +13,7 @@ class script(object):
     SOURCE_TXT = """<b>NOTE:</b>
 - Techno Mindz is A Open Source Project. 
 - You Can Find The Repo Link Below 👇👇
-- Source - <a href=https://t.me/Technomindzchat><b><i>Techno Mindz</i></b></a>  
+- Source - <a href=https://github.com/TechnoMindz/CommonBot_For_All><b><i>Techno Mindz</i></b></a>  
 
 <b>DEVS:</b>
 - <a href=https://t.me/TechnoMindzchat><b><i>Techno Mindz</i></b></a>"""
